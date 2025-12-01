26c93058605553a4abc7abd0ec4e6db5a7f13224
Siddhartha Kosti <siddhartha.kosti@raga.ai>
2025-12-01 13:59:18 +0530

    fix: add addColumnType and promptDetails in add column payload

--- ragaai_catalyst/dataset.py
+++ ragaai_catalyst/dataset.py
@@ -564,8 +564,10 @@ class Dataset:
             add_column_payload = {
                 "rowFilterList": [],
                 "columnName": column_name,
+                "addColumnType": "RUN_PROMPT",
                 "datasetId": dataset_id,
                 "variables": variables,
+                "promptDetails": {},
                 "promptTemplate": {
                     "textFields": text_fields,
                     "modelSpecs": {
